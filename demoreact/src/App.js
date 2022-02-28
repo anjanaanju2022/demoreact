84ecd8446dcdbb99fc456c24704a0a8f21e01c0c
anjana <anjanapg4@gmail.com>
2022-02-28 09:27:42 -0800

    initial commit

---
+++ demoreact/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          new react app using Github
+        </p>
+        <a
+          className="App-link"
+          href="https://www.cookingchanneltv.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Enter into the new world of cooking
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
